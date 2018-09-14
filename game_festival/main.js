05f6ea09891cf9cb3e33e1f414995edf5a103f47
MikatoFu31 <ksoft2015@gmail.com>
2018-09-14 14:15:33 +0900

    Added browser game

--- game_festival/main.js
+++ game_festival/main.js
@@ -13,6 +13,7 @@ window.onload = function() {
         var mousey;
         var direction=0;
         var fruit=new Array();
+        var pointup=new Array();
         var score=0;
         var label_score = new Label("スコア:"+score);
         var life=7;
@@ -28,10 +29,18 @@ window.onload = function() {
             fruit[i].visible=false;
             game.rootScene.addChild(fruit[i]);
         }
+        for(i=0;i<20;i++){
+            pointup[i]=new Label("");
+            pointup[i].visible=false;
+            pointup[i].font="Sawarabi Gothic bold 20px";
+            game.rootScene.addChild(pointup[i]);
+        }
+
+
         label_score.color="#303030";
         label_score.x=20;
         label_score.y=0;
-        label_score.font="20px";
+        label_score.font="Sawarabi Gothic bold 20px";
         game.rootScene.addChild(label_score);
 
         game.rootScene.backgroundColor  = '#88EEEE';
@@ -82,29 +91,65 @@ window.onload = function() {
                 for(i=0;i<10;i++){
                     if (fruit[i].visible==false){
                         fruit[i].visible=true;
-                        fruit[i].x=Math.random()*320;
+                        fruit[i].x=Math.random()*(240-32);
                         fruit[i].y=0;
+                        fruit[i].yspeed=Math.random()*8+3;
                         console.log("fruit appeared:"+i);
                         break;
                     }
                 }
             }
-        for(i=0;i<10;i++){
-            if(fruit[i].visible==true){
-                fruit[i].y+=6;
-                if (fruit[i].y>320){
-                    fruit[i].visible=false;
+        for(j=0;j<10;j++){
+            if(fruit[j].visible==true){
+                fruit[j].y+=fruit[j].yspeed;
+                if (fruit[j].y>320){
+                    fruit[j].visible=false;
                 }
-                if (me.intersect(fruit[i])){
-                    console.log("You touched a fruit");
-                    fruit[i].visible=false;
+                if (me.intersect(fruit[j]) & fruit[j].visible==true){
+                    console.log("You touched a fruit "+i+" in frame "+frames);
+                    add_pointup(pointup,"+10",fruit[j].x,fruit[j].y,20);
+                    fruit[j].visible=false;
                     score+=10;
                     label_score.text=("スコア:"+score);
                 }
             }
         }
 
+        nexttick_pointup(pointup);
+
         });
     }
     game.start();
+}
+
+
+function add_pointup(pointup,texttoshow,xpos,ypos,drawcount){
+    for (i=0;i<10;i++){
+        if(pointup[i].visible==false){
+            console.log("Point"+xpos);
+            pointup[i].text=texttoshow;
+            pointup[i].visible=true;
+            pointup[i].x=xpos;
+            pointup[i].y=ypos;
+/*            pointup[i].xs=xspeed;
+            pointup[i].ys=yspeed;*/
+            pointup[i].dc=drawcount;
+            break;
+        }
+    }
+
+}
+function nexttick_pointup(pointup){
+    for (i=0;i<10;i++){
+        if(pointup[i].visible==true){
+/*            pointup[i].x+=pointup[i].xspeed;
+            pointup[i].y+=pointup[i].yspeed;*/
+            pointup[i].dc--;
+            if (pointup[i].dc<=0){
+                console.log("Point disappeared");
+                pointup[i].visible=false;
+            }
+        }
+    }
+
 }
